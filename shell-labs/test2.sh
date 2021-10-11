94579cdbde040e1cf8772e7dec1676d2b2ea5d7c
Prince Stanley <ingjeanbaptiste@gmail.com>
2021-10-11 14:21:12 -0400

    Add test.sh file

---
+++ shell-labs/test2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+while read input_text
+do
+  case $input_text in
+    hello)
+        echo English ;;
+    howdy)
+        echo American ;;
+    gday)
+        echo Autralian ;;
+    bonjour)
+        echo French ;;
+    "guten tag")
+        echo German ;;
+    *)
+        echo Unkown Language: $input_text
+  esac
+
+done < myfile.txt
