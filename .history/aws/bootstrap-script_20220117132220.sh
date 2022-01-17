0e931bbe54b40da4f7539542509ca21ce2788e22
Jean Baptiste Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-01-17 13:49:42 -0500

    Add..

---
+++ .history/aws/bootstrap-script_20220117132220.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+yum install amazon-linux-extras httpd -y 
+amazon-linux-extras install php7.2 -y
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+cd /var/www/html
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+cp -r wordpress/* /var/www/html/
+rm -rf wordpress
+rm -rf latest.tar.gz
+chmod -R 755 /var/www/html/*
+chown -R apache:apache /var/www/html/*
+wget https://s3.amazonaws.com/bucketforwordpresslab-donotdelete/htaccess.txt 
+mv htaccess.txt .htaccess
+systemctl restart httpd
+
