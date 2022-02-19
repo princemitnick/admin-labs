eeb39129d6414b8b8c9797ac890354f350c39add
Jean Baptiste Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-02-19 10:26:34 -0500

    Reinialization

--- http-status-script.sh
+++ http-status-script.sh
@@ -9,7 +9,7 @@ check_certificate="--no-check-certificate"
 #
 http_code=`wget $check_certificate --spider -S ""$url"" 2>&1 | grep "HTTP/" | awk '{print $2}' | grep 200`
 
-#Tester si le code http est eqal a 200
+#Tester si le code http est egal a 200
 
 
 if [ "$http_code" == "200" ]
